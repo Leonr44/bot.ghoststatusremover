dcfc03f6a9fa659aee5c50ca2468089c4609a49a
Leonr44 <leon1256432@gmail.com>
2024-08-25 11:01:31 +1000

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["Watching Volcron-4","Thinking Knighttt"];
 
 
 let currentIndex = 0;
